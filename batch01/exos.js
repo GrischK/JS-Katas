3e9ce268f859b8890851ba00049835cb0febe197
GrischK <grischka.gorski@laposte.net>
2022-03-22 10:07:24 +0100

    exos JS commencés

--- batch01/exos.js
+++ batch01/exos.js
@@ -1,10 +1,23 @@
 // Ecrire une variable contenant ton age (number) et une constante contenant ta date de naissance (string)
 let age = 37;
 const dateOfBirth = "21/01/1985";
+
+console.log(age, dateOfBirth);
 // Ecrire un code qui t'affiche ton prénom si ton âge est pair (... Quoi ? Pourquoi pas ?!)
-
+if (age %2 === 0) {
+  console.log("Ton age est pair");
+} else {
+  console.log("Ton age est impair");
+};
 // Ecrire un code qui t'affiche ton prénom si ton âge est inférieur à 27, et ton nom dans le cas contraire
+const name = "Gorski";
+const firstName = "Grischka";
 
+if (age < 27) {
+  console.log(firstName);
+} else {
+  console.log(name);
+};
 // Ecrire un code qui me copie 10 lignes de "Je ne copierai pas du code bêtement" (oui, c'est du recyclage d'exos :p)
 
 // Ecrire une fonction getToto qui me renvoie la string "toto"
